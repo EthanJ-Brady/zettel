abc7f5fd15f34e45372290c9194b3236b80c8e47
Ethan Brady <git@ethanbrady.xyz>
2025-02-10 01:43:45 -0700

    Made it so the new file begins with the header of the title

--- src/new.rs
+++ src/new.rs
@@ -4,6 +4,9 @@ use std::io::prelude::*;
 pub fn new(title: &str) -> std::io::Result<()> {
     let file_name = format!("{title}.md");
     let mut file = File::create(file_name)?;
-    file.write_all(b"# Hello")?;
+
+    let file_text = format!("# {title}");
+    write!(file, "{}", file_text)?;
+
     Ok(())
 }
