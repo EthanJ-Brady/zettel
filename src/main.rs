7894e1c58c71c42714925434f4bfdeb5b4047934
Ethan Brady <git@ethanbrady.xyz>
2025-02-10 01:38:28 -0700

    Added a basic implementation of the new subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
 mod cli;
+mod new;
 
 use clap::Parser;
 use cli::Cli;
 use cli::Commands;
+use new::new;
+use std::io;
 
 fn main() {
     let args = Cli::parse();
 
     match &args.command {
-        Commands::New(args) => println!("{}", &args.title),
-        Commands::Find(_args) => {}
-        Commands::Open(_args) => {}
-        Commands::Append(_args) => {}
+        Commands::New(args) => new(&args.title).expect("Failed to create file"),
+        Commands::Find(_args) => dummy().expect("Failed to find"),
+        Commands::Open(_args) => dummy().expect("Failed to open"),
+        Commands::Append(_args) => dummy().expect("Failed to append"),
     }
+}
+
+fn dummy() -> Result<(), io::Error> {
+    Ok(())
 }
---
+++ src/new.rs
@@ -0,0 +1,9 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+pub fn new(title: &str) -> std::io::Result<()> {
+    let file_name = format!("{title}.md");
+    let mut file = File::create(file_name)?;
+    file.write_all(b"# Hello")?;
+    Ok(())
+}
