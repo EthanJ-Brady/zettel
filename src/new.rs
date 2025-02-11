b96815a3b5195b88c4bd0f90239dac31a6211bde
EthanJ-Brady <git@ethanbrady.xyz>
2025-02-10 17:40:20 -0700

    Updated the name of the file that gets created with the new command to add the zettel date at the beginning

--- src/new.rs
+++ src/new.rs
@@ -1,12 +1,23 @@
+use chrono::Local;
 use std::fs::File;
 use std::io::prelude::*;
 
 pub fn new(title: &str) -> std::io::Result<()> {
     let file_name = format!("{title}.md");
+    let file_name = file_name.replace(" ", "-");
+    let file_name = file_name.trim().to_lowercase();
+    let zettel_date = get_zettel_datetime();
+    let file_name = format!("{zettel_date}-{file_name}");
     let mut file = File::create(file_name)?;
 
-    let file_text = format!("# {title}");
+    let header = title;
+    let file_text = format!("# {header}");
     write!(file, "{}", file_text)?;
 
     Ok(())
+}
+
+fn get_zettel_datetime() -> String {
+    let date = Local::now();
+    date.format("%Y%m%d%H%M").to_string()
 }
