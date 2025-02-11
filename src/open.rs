19686d31b15ed0139b590240191cfbe928c07221
EthanJ-Brady <git@ethanbrady.xyz>
2025-02-10 21:32:25 -0700

    Factored out the opening file logic to its own function

--- src/open.rs
+++ src/open.rs
@@ -1,18 +1,22 @@
 use crate::find::find_first;
 use std::io;
+use std::path::PathBuf;
 use std::{env::var, fs::File, io::Read, process::Command};
 
 pub fn open(search_string: &str, dir: &str) -> Result<(), io::Error> {
     let dir_entry = find_first(search_string, dir);
+    open_file(dir_entry.path())
+}
 
+pub fn open_file(path: PathBuf) -> Result<(), io::Error> {
     let editor = var("EDITOR").unwrap();
     Command::new(editor)
-        .arg(&dir_entry.path())
+        .arg(&path)
         .status()
         .expect("Something went wrong");
 
     let mut editable = String::new();
-    File::open(&dir_entry.path())
+    File::open(&path)
         .expect("Could not open file")
         .read_to_string(&mut editable)?;
 
